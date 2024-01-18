02507330073990d1f9d1f66cd7f4f54748f991c5
Prosper Atu <atu.prosper@gmail.com>
2024-01-18 06:15:23 -0800

    created function to check digit

---
+++ is_digit.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+* is_digit - checks if all characters in a string are digits
+* @str: input string
+* Return: 0 if all characters are digits, else -1
+*/
+int is_digit(const char *str)
+{
+int i = 0;
+
+if (*str == '-')
+	i = 1;
+
+while (str[i] != '\0')
+{
+	if (!isdigit(str[i]))
+	return (-1);
+	i++;
+}
+
+return (0);
+}
+
