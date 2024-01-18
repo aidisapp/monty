5a5066394a78caece8dedaf672861d4ca310f059
Prosper Atu <atu.prosper@gmail.com>
2024-01-18 07:05:34 -0800

    created some opcodes

--- opcodes.c
+++ opcodes.c
@@ -1,6 +1,65 @@
 #include "monty.h"
 
 /**
+* add_node - adds a new node to the beginning of a doubly linked list
+* @head: pointer to the head of the linked list
+* @n: integer data for the new node
+* Return: 0 on success, -1 on failure
+*/
+int add_node(stack_t **head, int n)
+{
+stack_t *new_node = malloc(sizeof(stack_t));
+
+if (!head)
+	return (-1);
+
+if (!new_node)
+{
+	printf("Error: memory allocation failed\n");
+	return (-1);
+}
+new_node->n = n;
+
+if (*head == NULL)
+{
+	*head = new_node;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+}
+else
+{
+	new_node->next = *head;
+	(*head)->prev = new_node;
+	*head = new_node;
+}
+
+return (0);
+}
+
+/**
+* delete_node - deletes the last node in a doubly linked list
+* @list: pointer to the head node of the linked list
+*/
+void delete_node(stack_t *list)
+{
+stack_t *node_to_delete = NULL;
+
+if (list->next == NULL)
+{
+	node_to_delete = list;
+	list = NULL;
+}
+else
+{
+	node_to_delete = list;
+	list = list->next;
+	list->prev = NULL;
+}
+
+free(node_to_delete);
+}
+
+/**
 * push - Pushes an element to the stack.
 * @stack: Double pointer to the beginning of the stack.
 * @line_num: Line number in the Monty bytecodes file.
@@ -23,45 +82,19 @@ if (!add_node(stack, atoi(arg_token)))
 }
 
 /**
-* pall - Prints all values on the stack.
-* @stack: Pointer to the beginning of the stack.
-* @line_num: Line number in the Monty bytecodes file.
+* free_list - frees a doubly linked list with only int data, no strings
+* @head: pointer to the head of the list
 */
-void pall(stack_t **stack, __attribute__((unused)) unsigned int line_num)
+void free_list(stack_t **head)
 {
-stack_t *current = *stack;
+if (!head || !(*head))
+	return;
 
-while (current)
+while (*head && (*head)->next)
 {
-	printf("%d\n", current->n);
-	current = current->next;
+	*head = (*head)->next;
+	free((*head)->prev);
 }
-}
-
-/**
-* add_node - Adds a new node to the stack.
-* @stack: Double pointer to the beginning of the stack.
-* @n: Value to be added to the new node.
-* Return: Address of the new node, or NULL on failure.
-*/
-stack_t *add_node(stack_t **stack, int n)
-{
-stack_t *new_node = malloc(sizeof(stack_t));
-
-if (!new_node)
-	return (NULL);
-
-new_node->n = n;
-new_node->prev = NULL;
-
-if (!*stack)
-	new_node->next = NULL;
-else
-{
-	new_node->next = *stack;
-	(*stack)->prev = new_node;
+free(*head);
 }
 
-*stack = new_node;
-return (new_node);
-}
