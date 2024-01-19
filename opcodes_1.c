d3c8331a5214c44c60c21b8cbaa0fd1afc5458f1
Prosper Atu <atu.prosper@gmail.com>
2024-01-19 04:16:02 -0800

    created opcodes_1.c file

---
+++ opcodes_1.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * pint - opcode to print top node in stack
+ * @head: head node of list
+ * @line_num: line number bytecode
+ */
+void pint(stack_t **head, unsigned int line_num)
+{
+	if (!head || !*head)
+	{
+		fprintf(stderr, "L%u: can't pint, stack is empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+
+/**
+ * pop - opcode to remove node at front of list
+ * @head: head of linked list, at bottom of list
+ * @line_num: line number bytecode
+ */
+void pop(stack_t **head, unsigned int line_num)
+{
+	if (head == NULL || *head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop, stack is emoty\n", line_num);
+		free_list(head);
+		exit(EXIT_FAILURE);
+	}
+	else
+		delete_node(head);
+}
+
