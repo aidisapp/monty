f0808dc87f5d4ff02db1743901fe914c72a4f6a9
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-20 21:54:22 +0100

    implemented div opcode

--- opcodes_2.c
+++ opcodes_2.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
-* sub - Subtracts the top element of the stack from the second top element.
+* sub_op - Subtracts the top element of the stack from the second top element.
 * @head: Double pointer to the beginning of the stack.
 * @line_num: Line number in the Monty bytecodes file.
 */
-void sub(stack_t **head, unsigned int line_num)
+void sub_op(stack_t **head, unsigned int line_num)
 {
 if (!head || !*head || !(*head)->next)
 {
@@ -18,3 +18,27 @@ if (!head || !*head || !(*head)->next)
 delete_node(head);
 }
 
+/**
+* div_op - Divides the second top element of the stack by the top element.
+* @head: Double pointer to the beginning of the stack.
+* @line_num: Line number in the Monty bytecodes file.
+*/
+void div_op(stack_t **head, unsigned int line_num)
+{
+if (!head || !*head || !(*head)->next)
+{
+	fprintf(stderr, "L%u: can't div, stack too short\n", line_num);
+	free_list(head);
+	exit(EXIT_FAILURE);
+}
+
+if ((*head)->n == 0)
+{
+	fprintf(stderr, "L%u: division by zero\n", line_num);
+	free_list(head);
+	exit(EXIT_FAILURE);
+}
+
+(*head)->next->n /= (*head)->n;
+delete_node(head);
+}
