a344b079a347a91c5730a4f8ec395580516f84af
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-20 18:47:47 +0100

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -1,45 +1,45 @@
 #include "monty.h"
 
 /**
- * add_node - adds a new node to the beginning of a doubly linked list
- * @head: pointer to the head of the linked list
- * @n: integer data for the new node
- * Return: 0 on success, -1 on failure
- */
+* add_node - adds a new node to the beginning of a doubly linked list
+* @head: pointer to the head of the linked list
+* @n: integer data for the new node
+* Return: 0 on success, -1 on failure
+*/
 int add_node(stack_t **head, int n)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
+stack_t *new_node = malloc(sizeof(stack_t));
 
-	if (!head)
-		return (-1);
+if (!head)
+	return (-1);
 
-	if (!new_node)
-	{
-		printf("Error: memory allocation failed\n");
-		return (-1);
-	}
-	new_node->n = n;
+if (!new_node)
+{
+	printf("Error: memory allocation failed\n");
+	return (-1);
+}
+new_node->n = n;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-	}
-	else
-	{
-		new_node->next = *head;
-		(*head)->prev = new_node;
-		*head = new_node;
-	}
+if (*head == NULL)
+{
+	*head = new_node;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+}
+else
+{
+	new_node->next = *head;
+	(*head)->prev = new_node;
+	*head = new_node;
+}
 
-	return (0);
+return (0);
 }
 
 /**
- * delete_node - deletes the last node in a doubly linked list
- * @head: pointer to the head node of the linked list
- */
+* delete_node - deletes the last node in a doubly linked list
+* @head: pointer to the head node of the linked list
+*/
 void delete_node(stack_t **head)
 {
 	stack_t *node_to_del = NULL;
@@ -59,53 +59,54 @@ void delete_node(stack_t **head)
 }
 
 /**
- * free_list - frees an int linked list
- * @head: pointer to the head node of the doubly list
- */
+* free_list - frees an int linked list
+* @head: pointer to the head node of the doubly list
+*/
 void free_list(stack_t **head)
 {
-	stack_t *current = *head;
-	stack_t *next;
+if (!head || !(*head))
+	return;
 
-	while (current)
-	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
-
-	*head = NULL;
+while (*head && (*head)->next)
+{
+	*head = (*head)->next;
+	free((*head)->prev);
+}
+free(*head);
 }
 
 /**
- * push - Pushes an element to the stack.
- * @head: Double pointer to the beginning of the stack.
- * @line_num: Line number in the Monty bytecodes file.
- * @token: token character
- */
+* push - Pushes an element to the stack.
+* @head: Double pointer to the beginning of the stack.
+* @line_num: Line number in the Monty bytecodes file.
+* @token: token character
+*/
 void push(stack_t **head, unsigned int line_num, const char *token)
 {
-	if (!head || !is_digit(token))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_num);
-		free_list(head);
-		exit(EXIT_FAILURE);
-	}
-
+if (!head)
+	return;
+if (is_digit(token) == -1)
+{
+	printf("L%u: usage: push stack\n", line_num);
+	free_list(head);
+	exit(EXIT_FAILURE);
+}
+else
+{
 	if (add_node(head, atoi(token)) == -1)
 	{
-		fprintf(stderr, "Error: memory allocation failed\n");
-		free_list(head);
-		exit(EXIT_FAILURE);
+	free_list(head);
+	exit(EXIT_FAILURE);
 	}
+}
 }
 
 /**
- * pall - prints all nodes in the stack
- * @head: pointer to the head of the stack
- * @line_num: bytecode line number (unused in this function)
- * Return: if the stack is empty, it should not print anything
- */
+* pall - prints all nodes in the stack
+* @head: pointer to the head of the stack
+* @line_num: bytecode line number (unused in this function)
+* Return: if the stack is empty, it should not print anything
+*/
 
 void pall(stack_t **head, __attribute__((unused)) unsigned int line_num)
 {
