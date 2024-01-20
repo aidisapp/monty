8e52333350687583fc91669fc6db1d36b7641a44
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-20 22:22:19 +0100

    implemented pchar opcode

--- opcodes_2.c
+++ opcodes_2.c
@@ -86,3 +86,28 @@ if ((*head)->n == 0)
 (*head)->next->n %= (*head)->n;
 delete_node(head);
 }
+
+/**
+* pchar - Prints the char at the top of the stack.
+* @head: Double pointer to the beginning of the stack.
+* @line_num: Line number in the Monty bytecodes file.
+*/
+void pchar(stack_t **head, unsigned int line_num)
+{
+if (!head || !*head)
+{
+	fprintf(stderr, "L%u: can't pchar, stack empty\n", line_num);
+	free_list(head);
+	exit(EXIT_FAILURE);
+}
+
+if ((*head)->n < 0 || (*head)->n > 127)
+{
+	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_num);
+	free_list(head);
+	exit(EXIT_FAILURE);
+}
+
+putchar((*head)->n);
+putchar('\n');
+}
