46131b22108f6f5e959209d1ea2c8b3e14cbd15a
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-21 13:44:30 +0100

    implemented rotl opcode

--- opcodes_3.c
+++ opcodes_3.c
@@ -19,3 +19,26 @@ while (temp_node != NULL && temp_node->n != 0 && temp_node->n >= 0
 putchar('\n');
 }
 
+/**
+* rotl - Rotates the stack to the top.
+* @head: Double pointer to the beginning of the stack.
+* @line_num: Line number in the Monty bytecodes file.
+*/
+void rotl(stack_t **head, __attribute__((unused)) unsigned int line_num)
+{
+stack_t *temp_node;
+
+if (head == NULL || *head == NULL || (*head)->next == NULL)
+	return;
+
+temp_node = *head;
+
+while (temp_node->next != NULL)
+	temp_node = temp_node->next;
+
+temp_node->next = *head;
+(*head)->prev = temp_node;
+*head = (*head)->next;
+(*head)->prev = NULL;
+temp_node->next = NULL;
+}
